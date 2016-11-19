9018c9ddb10819c32b414c533bfc3999ed6cf0e0
Brian Foster <bdfoster89@gmail.com>
2016-11-18 20:42:09 -0500

    Delete more files

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.lodash = require("lodash");
-exports.async = require("async");
-var EventEmitter_1 = require("./EventEmitter");
-exports.EventEmitter = EventEmitter_1.default;
-var EventListener_1 = require("./EventListener");
-exports.EventListener = EventListener_1.default;
-//# sourceMappingURL=index.js.map
