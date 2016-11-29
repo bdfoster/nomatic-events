58d2f8f1ecc4837724fc9c45bdf2523ad4cf85ad
Brian Foster <bdfoster89@gmail.com>
2016-11-28 23:46:08 -0500

    Add more tests, enforce `once` setting

--- test/unit/EventListener.test.js
+++ test/unit/EventListener.test.js
@@ -26,10 +26,31 @@ describe('EventListener', function() {
         })
     });
 
-    describe('#execute', function() {
+    describe('#execute()', function() {
         it('should execute the callback given the event data', function() {
             listener.execute(this, true);
             expect(listenerResult).to.equal(true);
         })
+    });
+
+    describe("#close()", function() {
+        it("should remove itself from `emitter` (EventEmitter)", function() {
+            listener.close();
+            expect(listener.emitter.listeners.length).to.equal(0);
+        })
+    });
+
+    describe("#open()", function() {
+        it("should add itself back to `emitter` (EventEmitter)", function() {
+            listener.open(true);
+            expect(listener.emitter.listeners.length).to.equal(1);
+        });
+    });
+
+    describe("#once", function() {
+        it("should only execute the listener once and remove itself from `emitter`", function() {
+            listener.execute(this, true);
+            expect(listener.emitter.listeners.length).to.equal(0);
+        });
     })
-})
+});
